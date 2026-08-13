fe6c21b1c51ac8dc5d1fe22187397d3f836e0ac7
Midunavarshinisellamuthu <midunavarshini342@gmail.com>
2026-08-13 10:54:17 +0530

    Add optimized ToDo API

--- library_management/api.py
+++ library_management/api.py
@@ -53,3 +53,26 @@ def update_memberships():
     records = query.run(as_dict=True)
 
     return records
+
+@frappe.whitelist()
+def get_recent_todos():
+    todos = frappe.get_list(
+        "ToDo",
+        fields=["name", "description", "owner"],
+        order_by="creation desc",
+        limit_page_length=5
+    )
+
+    for todo in todos:
+        todo["email"] = frappe.db.get_value(
+            "User",
+            todo["owner"],
+            "email"
+        )
+
+    timestamp = frappe.utils.now()
+
+    return {
+        "timestamp": timestamp,
+        "records": todos
+    }
